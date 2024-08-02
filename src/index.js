74e4370d06b19197d5615fa6f96110a855ebe0e3
Nemo <nemoshen94@gmail.com>
2024-08-02 13:40:46 +0800

    WIP

--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 import alfy from "alfy";
 
 const data = await alfy.fetch('https://jsonplaceholder.typicode.com/posts');
-console.log(123010231023)
 
 const items = alfy
   .inputMatches(data, 'title')
